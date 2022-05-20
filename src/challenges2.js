7be6eeb9295837c67dfb02350c38319fc1bf3c5e
Guilherme Garcia <garciaguig@gmail.com>
2022-05-19 23:08:11 -0300

    Conclusão do DESAFIO 11 (Bônus)

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,53 @@
 // Desafio 11
-function generatePhoneNumber() {
-  // seu código aqui
+function generatePhoneNumber(phoneNumber) {
+
+  if (phoneNumber.length < 11 || phoneNumber.length > 11) {
+    return 'Array com tamanho incorreto.';
+  } 
+
+  for (index = 0; index < phoneNumber.length; index += 1){
+
+    let repeatedNumber = phoneNumber[index]
+    let repetitionCounter = 0;
+  
+    for (i = 0; i < phoneNumber.length; i += 1) {
+  
+      if (repeatedNumber === phoneNumber[i]) {
+        repetitionCounter += 1;
+      }
+    }
+  
+    if (repetitionCounter >= 3) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  for (index = 0; index < phoneNumber.length; index += 1) {
+
+    if (phoneNumber[index] < 0 || phoneNumber[index] > 9) {
+      return 'não é possível gerar um número de telefone com esses valores';
+    }
+  }
+
+  let numberGenerated = '';
+
+  for (index = 0; index < phoneNumber.length; index += 1) {
+
+    if (index === 0) {
+      numberGenerated += "("+phoneNumber[index];
+    
+    } else if (index === 1) {
+      numberGenerated += phoneNumber[index]+") ";
+  
+    } else if (index === 6) {
+      numberGenerated += phoneNumber[index]+"-";
+  
+    } else {
+      numberGenerated += phoneNumber[index] ;
+
+    }
+  }
+  return numberGenerated;
 }
 
 // Desafio 12
