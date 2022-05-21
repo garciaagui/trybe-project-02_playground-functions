107a14fbd882ca50e5f3e2a2d2613076fe6b7afc
Guilherme Garcia <garciaguig@gmail.com>
2022-05-20 22:08:09 -0300

    Conclusão do DESAFIO 13 (Bônus)

--- src/challenges2.js
+++ src/challenges2.js
@@ -99,8 +99,23 @@ function triangleCheck(lineA, lineB, lineC) {
 }
 
 // Desafio 13
-function hydrate() {
-  // seu código aqui
+function hydrate(drinksDrunk) {
+
+  let extractNumbers = drinksDrunk.match(/\d+/g).map(Number);
+  let drinksNumber = 0;
+  let glassesWater = 0;
+  
+  for (i = 0; i < extractNumbers.length; i += 1) {
+    drinksNumber += extractNumbers[i];
+  }
+
+  if (drinksNumber === 1) {
+    glassesWater = drinksNumber + ' copo de água'
+  } else {
+    glassesWater = drinksNumber + ' copos de água'
+  }
+
+  return glassesWater;
 }
 
 module.exports = {
